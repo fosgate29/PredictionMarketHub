e96d931f43e36a5a196ed33378f1bc0ea6571ed2
fosgate29 <luizhamilton29@gmail.com>
2017-09-11 18:04:00 -0300

    Update PredictionMarketHub.sol

--- contracts/PredictionMarketHub.sol
+++ contracts/PredictionMarketHub.sol
@@ -1,7 +1,6 @@
-
 pragma solidity ^0.4.11;
 
-
+ 
 import "./PredictionMarket.sol";
 
 contract PredictionMarketHub is Stoppable{
@@ -9,11 +8,11 @@ contract PredictionMarketHub is Stoppable{
     address[] public predictionMarkets;
     mapping(address => bool) public predictionMarketExists;
     
-    event LogNewPredictionMarket(address owner, address newPredictionMarket, 
+    event LogNewPredictionMarket(address owner, address admin, address newPredictionMarket, 
                                   uint winOdds, uint multiplier, uint minimunBet, uint maximunBet);
-    event LogPredictionMarketStopped(address sender, address newPredictionMarket);
-    event LogPredictionMarketStarted(address sender, address newPredictionMarket);
-    event LogPredictionMarketNewOwner(address sender, address newPredictionMarket, address newOwner);
+    event LogPredictionMarketStopped(address sender, address predictionMarket);
+    event LogPredictionMarketStarted(address sender, address predictionMarket);
+    event LogPredictionMarketNewOwner(address sender, address predictionMarket, address newOwner);
     
     modifier onlyIfPredictionMarket(address predictionMarket) {
         require(predictionMarketExists[predictionMarket] == true);
@@ -34,20 +33,21 @@ contract PredictionMarketHub is Stoppable{
     {
         PredictionMarket trustedPredictionMarket;   //sempre marcar como trusted o contrato que vc conhece
                                                     //ajuda na hora do code review
-        trustedPredictionMarket = new PredictionMarket( winOdds, multiplier, 
+        trustedPredictionMarket = new PredictionMarket( msg.sender, winOdds, multiplier, 
                                                minimunBet, maximunBet);
         predictionMarkets.push(trustedPredictionMarket);
         predictionMarketExists[trustedPredictionMarket] = true;
 
-        LogNewPredictionMarket(msg.sender, trustedPredictionMarket,  winOdds, multiplier, 
-                                               minimunBet, maximunBet);
+        //Hub contract is the owner and admin is who sent the msg
+        LogNewPredictionMarket(owner, msg.sender, trustedPredictionMarket, winOdds, 
+                                        multiplier, minimunBet, maximunBet);
         return trustedPredictionMarket;
     }
     
     function stopPredictionMarket(address predictionMarket)
         onlyOwner
         onlyIfPredictionMarket(predictionMarket)
-        returns(bool success)
+        returns(bool success) 
     {
         PredictionMarket trustedPredictionMarket = PredictionMarket(predictionMarket);
         LogPredictionMarketStopped(msg.sender, predictionMarket);
