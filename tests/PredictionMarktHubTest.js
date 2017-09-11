194f9d1057037e0421c54696da4604104b877626
fosgate29 <luizhamilton29@gmail.com>
2017-09-11 18:12:28 -0300

    Update PredictionMarktHubTest.js

--- tests/PredictionMarktHubTest.js
+++ tests/PredictionMarktHubTest.js
@@ -92,10 +92,10 @@ contract('PredictionMarketHub', function(accounts) {
   });
 
   it("...should create a new Prediction Market", function() {
-      return contract.createPredictionMarket(1,1,1,9999, {from: accounts[0]})
+      return contract.createPredictionMarket(1,1,1,9999, {from: accounts[0], gas:3000000})
         .then(function(tx) {            
             var address = tx.logs[0].args.newPredictionMarket;
-            return contract.getPredictionMarketCount.call({from: accounts[0]})            
+            return contract.getPredictionMarketCount.call({from: accounts[0], gas:3000000})            
             .then(function(num){
               assert.equal(num.toString(10),1, "Prediction Market wasn't created correctly");
               return contract.predictionMarketExists(address)
@@ -107,37 +107,121 @@ contract('PredictionMarketHub', function(accounts) {
   });
 
   it("...should stop and start again a new Prediction Market", function() {
-      return contract.createPredictionMarket(1,1,1,9999, {from: accounts[0]})
+      return contract.createPredictionMarket(1,1,1,9999, {from: accounts[0], gas:3000000})
         .then(function(tx) {
             var address = tx.logs[0].args.newPredictionMarket;
-            return contract.stopPredictionMarket.call(address, {from: accounts[0]})            
+            return contract.stopPredictionMarket.call(address, {from: accounts[0], gas:3000000})            
             .then(function(stopOk){
-              assert.isTrue(stopOk, "Prediction Market didn't stop");  
-              return contract.startPredictionMarket.call(address, {from: accounts[0]})  
+              assert.isTrue(stopOk, "Prediction Market didn't stop"); 
+              return contract.startPredictionMarket.call(address, {from: accounts[0], gas:3000000})            
               .then(function(startOk){
-                assert.isTrue(startOk, "Prediction Market didn't start");
-              })            
-            })           
+                assert.isTrue(startOk, "Prediction Market didn't start"); 
+              })
+            })                    
       });
   });
 
 
   it("...should check isAdmin.", function() {
-      return contract.createPredictionMarket(1,1,1,9999, {from: accounts[0]})
+      return contract.createPredictionMarket( 1,1,1,9999, {from: accounts[2], gas:3000000})
         .then(function(tx) {            
             var address = tx.logs[0].args.newPredictionMarket;
             var abi = PredictionMarket.abi;          
             return  web3.eth.contract(abi).at(address, function (err, pm_contract) {
-              return contract.owner.call()
-               .then(function(hub_owner ){
-                console.log('Hub owner: ' + hub_owner);  
-                return pm_contract.owner((error, pm_owner) => {
-                    console.log("PM  owner: "+pm_owner);
-                 })              
+              return pm_contract.isAdmin.call(accounts[2], (error, isAdmin) => {
+                assert.isTrue(isAdmin, "Account 2 is not Admin");
+                return pm_contract.isAdmin.call(accounts[0], (error, isNotAdmin) => {
+                  assert.isFalse(isNotAdmin, "Account 0 is Admin");
+                })
+               })              
+            })     
+      });
+  });
+
+  it("...should add a question, bet and withdrawn.", function() {
+      return contract.createPredictionMarket( 1,1,1,9999, {from: accounts[0], gas:3000000})
+        .then(function(tx) {            
+            var address = tx.logs[0].args.newPredictionMarket;
+            var abi = PredictionMarket.abi;       
+            return  web3.eth.contract(abi).at(address, function (err, pm_contract) {
+              return pm_contract.addQuestion(60,{from: accounts[0], gas:3000000} , (error, success) => {
+                return pm_contract.addQuestion(10,{from: accounts[0], gas:3000000} , (error, success) => {
+                  return pm_contract.questionIdAt(0, (err, q) => { 
+                    var questionId = q; 
+                    return pm_contract.getQuestionsCount.call( (err, num) =>{ 
+                      assert.equal(num, 2, "Question count isn't 2.") 
+                      return pm_contract.bet(questionId, true, {from: accounts[1], value:90000000000, gas:3000000}, (error, betOk) => { 
+                        assert.isOk(betOk, "Bet wasn't made."); 
+                        return pm_contract.bet(2, true, {from: accounts[2], value:55555555, gas:3000000}, (error, bet2Ok) => { 
+                          var user2Balance_Initial = web3.eth.getBalance(accounts[2]).toNumber();
+                          assert.isOk(betOk, "Bet wasn't made.") ;
+                          return pm_contract.closeQuestionForBets(2, {from: accounts[0], gas:3000000}, (error, questionIsClosed) => { 
+                            return pm_contract.setQuestionAnswer(2, {from: accounts[0], gas:3000000}, (error, answerOk) => { 
+                              assert.isOk(answerOk, "Question wasn't answered.") ;
+                              return pm_contract.withdrawBet(2, {from: accounts[2], gas:3000000}, (error, withdrawOk) => { 
+                                var user2Balance_afterWithdraw = web3.eth.getBalance(accounts[2]).toNumber();
+                                assert.isAbove(user2Balance_afterWithdraw, user2Balance_Initial , "Withdrawn wasn't made.") ;
+                              })
+                            })
+                          })
+                        })
+                      })
+                    })
+                  }) 
+                })    
               })
             })     
       });
   });
 
+  it("...should add a trusted user and trusted user answer the question.", function() {
+      return contract.createPredictionMarket( 1,1,1,9999, {from: accounts[0], gas:3000000})
+        .then(function(tx) {            
+            var address = tx.logs[0].args.newPredictionMarket;
+            var abi = PredictionMarket.abi;       
+            return  web3.eth.contract(abi).at(address, function (err, pm_contract) {
+              return pm_contract.addQuestion(60,{from: accounts[0], gas:3000000} , (error, success) => {
+                var questionId = 1; //it is incremental
+                return pm_contract.setUserIsTrusted(accounts[1], true, {from: accounts[0], gas:3000000}, (error, userTrustedOk) => { 
+                  assert.isOk(userTrustedOk);
+                  return pm_contract.isUserTrusted.call(accounts[1], {from: accounts[1], gas:3000000}, (error, isUserTrusted) => { 
+                    assert.isTrue(isUserTrusted, "Account[1] is not trusted.");
+                    return pm_contract.closeQuestionForBets(questionId, {from: accounts[1], gas:3000000}, (error, questionIsClosed) => { 
+                      return pm_contract.setQuestionAnswer(questionId, {from: accounts[1], gas:3000000}, (error, answerOk) => { 
+                        assert.isOk(answerOk, "Answer wasn't set.");
+                        return pm_contract.QuestionsMapping.call(questionId, {from: accounts[1], gas:3000000}, (error, question) => { 
+                          var resultRoll = question[1].toString(10);
+                          assert.isAbove(resultRoll,0, "Question answer is still zero");
+                        })
+                      })
+                    })
+                  })
+                })   
+              })
+            })     
+      });
+  });
+
+  it("...should fail if user tries to bet in a stopped Prediction Market", function() {
+    return contract.createPredictionMarket( 1,1,10000,9999, {from: accounts[0], gas:3000000})
+      .then(function(tx) {            
+          var address = tx.logs[0].args.newPredictionMarket;
+          var abi = PredictionMarket.abi;       
+          return  web3.eth.contract(abi).at(address, function (err, pm_contract) {
+            return pm_contract.addQuestion(60,{from: accounts[0], gas:3000000} , (error, success) => {
+              var questionId = 1; //it is incremental
+              return contract.stopPredictionMarket(address, {from: accounts[0], gas:3000000})            
+              .then(function(txs){
+                var switchOff = txs.logs[1].args.switchSetting;
+                assert.isFalse(switchOff, "Prediction Market didn't stop");                 
+                 return expectedExceptionPromise(function () {
+                    return pm_contract.bet(questionId, true, {from: accounts[2], value:1 ,gas:3000000});     
+                      },
+                  3000000);
+              })   
+            })
+          })     
+    });
+  });
 
 })
